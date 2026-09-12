70b176976dbd63677f68301ba6f1a6fc08f6e29a
flameztech01 <flameztech01@gmail.com>
2026-09-12 13:33:38 +0100

    localhost

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
   "http://localhost:8000",
+  "http://localhost",
+  "https://localhost",
   "https://flanorx.onrender.com",
   "https://flanorx.vercel.app",
   "https://flanorx.com",
